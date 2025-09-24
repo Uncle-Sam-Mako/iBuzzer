ef6580fe36233ecab8d871522ee8b1f7cbbfe663
Uncle-Sam-Mako <bwsamuel6@gmail.com>
2025-09-24 14:26:40 +0200

    feat(server) : handle users connection et disconnection

--- server/index.js
+++ server/index.js
@@ -12,6 +12,42 @@ const wsServer = new WebSocketServer({server});
 
 const port = 8000;
 
+//When a user log out
+const handleClose = uuid => {
+
+    console.log(`User ${users[uuid].username} has gone`);
+
+    delete connections[uuid];
+    delete users[uuid];
+
+    //broadcast();
+}
+
+
+wsServer.on('connection', (connection, request) => {
+
+    const { username } = url.parse(request.url, true).query;
+    const uuid = uuidv4();
+
+    connections[uuid] = connection;
+
+    users[uuid] = {
+        username : username,
+        state : {
+
+        }
+    }
+    //When a connection is closed
+    connection.on("close", () => handleClose(uuid));
+
+
+
+
+
+    console.log(uuid);
+    console.log(username);
+})
+
 //connections list
 const connections = {};
 const users = {};
